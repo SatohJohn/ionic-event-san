50e44fa2d010f5006f5f4dcd5a4541ecbc0903e0
john <sato.keita@furyu.jp>
2018-01-27 16:34:59 +0900

    bookmark機能を追加した

--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { EventProvider } from '../../providers/event/event';
+import { BookmarkProvider } from '../../providers/bookmark/bookmark';
 
 /**
  * Generated class for the EventDetailPage page.
@@ -20,7 +21,7 @@ export class EventDetailPage {
 
   event: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public eventProvider: EventProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public eventProvider: EventProvider, public toastCtrl: ToastController, public bookmarkProvider: BookmarkProvider) {
   }
 
   ionViewDidLoad() {
@@ -33,4 +34,13 @@ export class EventDetailPage {
     }
   }
 
+  doBookmark() {
+    this.bookmarkProvider.put(this.event).then(() => {
+      const toast = this.toastCtrl.create({
+        message: 'イベントをブックマークしました。',
+        duration: 1500
+      });
+      toast.present();
+    })
+  }
 }
